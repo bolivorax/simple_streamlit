78a803cb706be41ef5bfe689d6561a96c2adb90d
bolivorax <65345352+bolivorax@users.noreply.github.com>
2022-09-06 09:48:44 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 09:21:01 2022
+
+@author: frederic
+"""
+
+import streamlit as st
+import pandas as pd
+
+st.title("A Simple Streamlit Web App")
+name = st.text_input("Enter your name", '')
+st.write(f'Hello {name}!')
+x = st.slider('Select an integer x', 0, 10, 1)
+y = st.slider('Select an integer y', 0, 10, 1)
+df = pd.DataFrame({'x': [x], 'y': [y] , 'x + y': [x + y]}, index = ['addition row'])
+st.write(df)
